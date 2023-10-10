1c587343b1f2e25b11a4cc1c31da1feca434b38b
Denchevv <145829367+Denchevv@users.noreply.github.com>
2023-10-10 20:43:41 +0300

    New Upload

---
+++ CSharp-Fundamentals/03Arrays-Lab/01DaysOfWeek/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] days =
+{
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+};
+
+int num = int.Parse(Console.ReadLine());
+
+if (num < 1 || num > 7)
+{
+    Console.WriteLine("Invalid day!");
+}
+else
+{
+    Console.WriteLine(days[num - 1]);
+}
---
+++ CSharp-Fundamentals/03Arrays-Lab/02PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[] numbers = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    int newNumber = int.Parse(Console.ReadLine());
+    numbers[i] = newNumber;
+}
+
+for (int i = numbers.Length - 1; i >= 0; i--)
+{
+    Console.Write(numbers[i] + " ");
+}
